2bd0864f4073169409267c83cbc7d5e17cb0984c
Divya-SRM <divyadiya6497@gmail.com>
2021-07-31 17:37:54 +0530

    Revert "Week2ex"
    This reverts commit 928bf90e2a3148e76b94f90fdad44cc5236be387.

--- Week2/Inheritance/src/main/java/com/example/demo/Repo/OrderRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.Order;
-
-public interface OrderRepo extends JpaRepository<Order,Integer>{
-
-}
